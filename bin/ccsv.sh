b3af04b3e078412fc666e11e46add077e9127aa8
Taoufik El Aoumari <taoufix@gmail.com>
2020-07-15 10:18:58 +0200

    Create ccsv.sh

---
+++ bin/ccsv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cols() {
+    sed 's/;/;│/g' "$1" | column -s"," -t -n
+}
+
+if [[ -n "$2" ]]; then
+    header="$(cols "$1" | head -n 1)"
+    echo "${header}"
+    echo "${header}" | sed 's/[^│]/─/g; s/│/┼/g'
+    cols "$1" | grep --color "$2"
+else
+    cols "$1"
+fi
