9e8569dcfe32f2e6b04deab37e51740da86fa848
Taoufik El Aoumari <taoufix@gmail.com>
2020-07-15 10:33:14 +0200

    Create .env-ssh.conf.py

---
+++ bin/.env-ssh.conf.py
@@ -0,0 +1,12 @@
+WEB_BROWSER="google-chrome"
+
+# colors  white, green, cyan, magenta, yellow, red
+ENVS=[
+  {
+    'title': "DEV       )                 ", 'color': white, 'servers': [
+      {'title':"Web App        ", 'host': "https://localhost/"},
+      {'title':"Front          ", 'host': "host1"},
+      {'title':"Back      ", 'host': "host2"},
+    ]
+  },
+]
