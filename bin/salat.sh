5148c3dead71894a25ac25c3945d810523a6ebad
Taoufik EL AOUMARI <taoufik.el-aoumari@ratp.fr>
2023-09-28 16:05:12 +0200

    Switched to NG

--- bin/salat.sh
+++ bin/salat.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 (
   echo "Subh,Duhr,Asr,Maghrib,Isha"
-  curl -s https://mawaqit.net/ar/ascmi-moulineaux \
+  curl -s https://mawaqit.net/ar/mosquee-de-noisy-le-grand-noisy-le-grand \
     | grep -E -o '"times":\[[^a]+\]' \
     | sed 's/"//g;  s/times:\[//; s/\]//'
 ) | column -s, -t
