c760779faff472d50df5660df1ccc7d8560297b7
Taoufik El Aoumari <t-elaoumari@presta.artefrance.fr>
2024-04-23 13:52:09 +0200

    print mosque

--- bin/salat.sh
+++ bin/salat.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+mosque="mosquee-de-noisy-le-grand-noisy-le-grand"
+echo "* $mosque"
+
 (
   echo "Subh,Duhr,Asr,Maghrib,Isha"
-  curl -s https://mawaqit.net/ar/mosquee-de-noisy-le-grand-noisy-le-grand \
+  curl -s https://mawaqit.net/ar/$mosque \
     | grep -E -o '"times":\[[^a]+\]' \
     | sed 's/"//g;  s/times:\[//; s/\]//'
 ) | column -s, -t
