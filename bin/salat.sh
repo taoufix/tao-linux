d7435e30790ae1bc06658c38b66bd7daf6179b73
Taoufik El Aoumari <t-elaoumari@presta.artefrance.fr>
2024-05-07 14:09:45 +0200

    Kanz prayer times

--- bin/salat.sh
+++ bin/salat.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+mawaqit() {
+
 mosque="mosquee-de-noisy-le-grand-noisy-le-grand"
-echo "* $mosque"
+echo "$(date +%R) - $mosque"
 
 (
   echo "Subh,Duhr,Asr,Maghrib,Isha"
@@ -10,3 +12,19 @@ echo "* $mosque"
     | grep -E -o '"times":\[[^a]+\]' \
     | sed 's/"//g;  s/times:\[//; s/\]//'
 ) | column -s, -t
+
+}
+
+
+kanz() {
+    city="noisy-le-grand/93160"
+    echo "$(date +%R) - $city"
+    echo -e "Subh\tChour\tDuhr\tAsr\tMaghrib\tIsha"
+    (
+#	echo -e "Subh\nDuhr\nAsr\nMaghrib\nIsha"
+	curl -s "https://www.al-kanz.org/horaire-de-priere/${city}/" | egrep 'td class="horaire-' | sed 's/.*>\(.*\)<.*/\1/'
+    ) | column
+}
+
+kanz
+
